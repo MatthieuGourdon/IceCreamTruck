c96b5e5b5af48769beedf86dac79edcae3a76958
MatthieuGourdon <matthieu.gourdon@cpe.fr>
2021-08-02 11:00:48 +0200

    init

---
+++ inventory/src/test/java/com/ict/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
